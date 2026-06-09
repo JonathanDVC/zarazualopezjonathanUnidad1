a902e91b97f7f78bf545723f876faf638ad9a3bf
Jonathan Zarazua <jonathanzarazualopez@gmail.com>
2026-06-08 20:51:28 -0600

    Host local

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,10 +2,10 @@
 
 class Conexion {
 
-    private $host = "sql113.infinityfree.com";
-    private $usuario = "if0_42134991";
-    private $password = "Sjzd9un8kG";
-    private $bd = "if0_42134991_dwp8idgsb";
+    private $host = "localhost";
+    private $usuario = "root";
+    private $password = "";
+    private $bd = "unidad1";
 
     public function conectar() {
 
