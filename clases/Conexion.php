9a249a982f7e1fbd59687cac84f093fe6d153b5d
Jonathan Zarazua <jonathanzarazualopez@gmail.com>
2026-06-08 20:47:03 -0600

    Cambio de host

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,10 +2,10 @@
 
 class Conexion {
 
-    private $host = "localhost";
-    private $usuario = "root";
-    private $password = "";
-    private $bd = "unidad1";
+    private $host = "sql113.infinityfree.com";
+    private $usuario = "if0_42134991";
+    private $password = "Sjzd9un8kG";
+    private $bd = "if0_42134991_dwp8idgsb";
 
     public function conectar() {
 
